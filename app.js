c4a5589a28c3423d52c93c16584b5fcb1a48e008
haleydkeith <haleydkeith0905@gmail.com>
2020-06-15 09:55:47 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,116 @@
+slowMath
+  .add(6, 2)
+  .then((result) => {
+    console.log(result);
+    return result;
+  })
+
+  .then((result) => {
+    slowMath
+      .multiply(result, 2)
+      .then((result) => {
+        console.log(result);
+        return result;
+      })
+
+      .then((result) => {
+        slowMath
+          .divide(result, 4)
+          .then((result) => {
+            console.log(result);
+            return result;
+          })
+
+          .then((result) => {
+            slowMath
+              .subtract(result, 3)
+              .then((result) => {
+                console.log(result);
+                return result;
+              })
+
+              .then((result) => {
+                slowMath
+                  .add(result, 98)
+                  .then((result) => {
+                    console.log(result);
+                    return result;
+                  })
+
+                  .then((result) => {
+                    slowMath
+                      .remainder(result, 2)
+                      .then((result) => {
+                        console.log(result);
+                        return result;
+                      })
+
+                      .then((result) => {
+                        slowMath
+                          .multiply(result, 50)
+                          .then((result) => {
+                            console.log(result);
+                            return result;
+                          })
+
+                          .then((result) => {
+                            slowMath
+                              .remainder(result, 40)
+                              .then((result) => {
+                                console.log(result);
+                                return result;
+                              })
+
+                              .then((result) => {
+                                slowMath
+                                  .add(result, 32)
+                                  .then((result) => {
+                                    console.log(result);
+                                    return result;
+                                  })
+
+                                  .then((result) => {
+                                    console.log(
+                                      `The final result is ${result}.`
+                                    );
+                                  })
+
+                                  .catch((err) => {
+                                    console.log(err);
+                                  });
+                              });
+                          });
+                      });
+                  });
+              });
+          });
+      });
+  });
+
+const doMath = async () => {
+  try {
+    let result = await slowMath.add(6, 2);
+    console.log(result);
+    result = await slowMath.multiply(result, 2);
+    console.log(result);
+    result = await slowMath.divide(result, 4);
+    console.log(result);
+    result = await slowMath.subtract(result, 3);
+    console.log(result);
+    result = await slowMath.add(result, 98);
+    console.log(result);
+    result = await slowMath.remainder(result, 2);
+    console.log(result);
+    result = await slowMath.multiply(result, 50);
+    console.log(result);
+    result = await slowMath.remainder(result, 40);
+    console.log(result);
+    result = await slowMath.add(result, 32);
+    console.log(result);
+    console.log(`The final result is ${result}.`);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+doMath();
